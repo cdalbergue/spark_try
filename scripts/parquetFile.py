85fb6fa0922d7e81ee07cf5c9f659d61a86dad5d
Clément Dalbergue <clement@inchbase.com>
2016-10-04 15:48:15 +0200

    add parquetFile reader

---
+++ scripts/parquetFile.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from pyspark import SparkContext
+from pyspark import SparkConf
+from pyspark.sql import SQLContext, Row
+# import regex
+import re
+
+if __name__ == '__main__':
+  # conf est l'objet permettant de setter tous les paramètre d'execution de spark
+  conf = SparkConf()
+  # par exemple le nom de l'application
+  conf.setAppName("tp2 esiea")
+  # maintenant on va définir un CONTEXT spark nous permettant de signifier à python qu'on execute spark map reduce
+  sc= SparkContext(conf = conf)
+  # create sql context
+  sqlContext = SQLContext(sc)
+
+  # Read in the Parquet file created above. Parquet files are self-describing so the schema is preserved.
+  # The result of loading a parquet file is also a DataFrame.
+  parquetFile = sqlContext.read.parquet("users.parquet")
+
+  print(parquetFile)
