cc083e4e838990e090eacbe3b51802e3eba9359f
Clément Dalbergue <clement@inchbase.com>
2016-10-04 15:20:46 +0200

    add conf and main function

--- scripts/create_data_frame.py
+++ scripts/create_data_frame.py
@@ -1,23 +1,33 @@
+from pyspark import SparkContext
+from pyspark import SparkConf
 from pyspark.sql import SQLContext
 # import regex
 import re
-# create sql context
-sqlContext = SQLContext(sc)
 
-#  laod file
-lines = sc.textFile("../data/users.csv")
-# create the part for each line, colsep="\t" (with regexp)
-parts = lines.map(lambda line: re.split(r'\t', line))
+if __name__ == '__main__':
+    # conf est l'objet permettant de setter tous les paramètre d'execution de spark
+    conf = SparkConf()
+    # par exemple le nom de l'application
+    conf.setAppName("tp2 esiea")
+    # maintenant on va définir un CONTEXT spark nous permettant de signifier à python qu'on execute spark map reduce
+    sc= SparkContext(conf = conf)
+	# create sql context
+	sqlContext = SQLContext(sc)
 
-# map each line
-# UserID,Gender,Age,Occupation,Zip-code
-users = parts.map(lambda array: Row(userid=array[0], gender=array[1], age=int(array[2]), occupation=array[3], zipcode=string(array[4])))
+	#  laod file
+	lines = sc.textFile("../data/users.csv")
+	# create the part for each line, colsep="\t" (with regexp)
+	parts = lines.map(lambda line: re.split(r'\t', line))
 
-# map each line
-schemaUser = sqlContext.createDataFrame(users)
+	# map each line
+	# UserID,Gender,Age,Occupation,Zip-code
+	users = parts.map(lambda array: Row(userid=array[0], gender=array[1], age=int(array[2]), occupation=array[3], zipcode=string(array[4])))
 
-# create a temporary table
-schemaUser.registerTempTable("users")
+	# map each line
+	schemaUser = sqlContext.createDataFrame(users)
 
-# create parquet
-schemaUser.write.parquet("users.parquet")
+	# create a temporary table
+	schemaUser.registerTempTable("users")
+
+	# create parquet
+	schemaUser.write.parquet("users.parquet")
