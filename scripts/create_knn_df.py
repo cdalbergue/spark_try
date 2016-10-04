1a5ab016246ef9ef63b007b08562d66465c52038
Clément Dalbergue <clement@inchbase.com>
2016-10-04 17:50:40 +0200

    add creation of parquet

--- scripts/create_knn_df.py
+++ scripts/create_knn_df.py
@@ -16,11 +16,33 @@ if __name__ == '__main__':
   # create sql context
   sqlContext = SQLContext(sc)
 
-  #  laod files
-  users = sc.textFile("users.csv")
-  ratings = sc.textFile("ratings.csv")
-  movies = sc.textFile("movies.csv")
+  # Read in the Parquet file created above. Parquet files are self-describing so the schema is preserved.
+  # The result of loading a parquet file is also a DataFrame.
+  parquetFileUsers = sqlContext.read.parquet("users.parquet")
+  parquetFileUsers.registerTempTable("users");
 
-  # create the part for each line, colsep="\t" (with regexp)
-  users_parts = users.map(lambda line: re.split(r'\t', line))
-  print(users_parts)
+  parquetFileMovies = sqlContext.read.parquet("movies.parquet")
+  parquetFileMovies.registerTempTable("movies");
+
+  parquetFileRatings = sqlContext.read.parquet("ratings.parquet")
+  parquetFileRatings.registerTempTable("ratings");
+
+  query = "select ratings.userid,"+
+        "count(*) AS `TOTAL`,"+
+        "sum(case rating when 5 then 1 else 0 end) AS `5 STARS`,"+
+        "sum(case rating when 4 then 1 else 0 end) AS `4 STARS`,"+
+        "sum(case rating when 3 then 1 else 0 end) AS `3 STARS`,"+
+        "sum(case rating when 2 then 1 else 0 end) AS `2 STARS`,"+
+        "sum(case rating when 1 then 1 else 0 end) AS `1 STARS`,"+
+        "mean(rating) AS `MOY STARS`,"+
+        "count(rating)/12 AS `moy_months`,"+
+        "users.age,"+
+        "case gender when 'M' then 0 else 1 end"+
+        "from ratings"+
+        "left join users on users.userid = ratings.userid"+
+        "group by userid, age, gender"
+
+  knn_data = sqlContext.sql(query)
+  knn_df = sqlContext.createDataFrame(knn_data)
+  knn_df.registerTempTable("knn_data")
+  knn_df.write.parquet("knn_data.parquet")
